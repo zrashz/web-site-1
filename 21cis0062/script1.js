e79cb399f321df73c8c23f46f90d910aaeea39c7
zrashz <152951952+zrashz@users.noreply.github.com>
2024-01-24 12:22:59 +0530

    Add files via upload

---
+++ 21cis0062/script1.js
@@ -0,0 +1,5 @@
+document.getElementById("").addEventListener('click', function() {
+    // Change the URL to the desired webpage
+    window.location.href = 'file:///C:/Users/Acer/Desktop/21cis0062/index.html';
+  });
+  
